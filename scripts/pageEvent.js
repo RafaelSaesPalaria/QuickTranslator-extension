479b03b9ed4f2caa8411499493f165765d0f9b2d
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2024-05-09 17:20:23 -0300

    project is able to select the context

---
+++ scripts/pageEvent.js
@@ -0,0 +1,16 @@
+var contextMenuItem = {
+    "id": "quickTranslator",
+    "title": `Quick Translator`,
+    "contexts": ["selection"]
+}
+chrome.contextMenus.removeAll(function() {
+    chrome.contextMenus.create(contextMenuItem);
+});
+
+chrome.contextMenus.onClicked.addListener(function (clickData) {
+    let language = navigator.language || navigator.userLanguage
+    let text = clickData.selectionText
+
+    let prompt = `Translate everything i say to ${language}:\n ${text}`
+    console.log(prompt)
+})
