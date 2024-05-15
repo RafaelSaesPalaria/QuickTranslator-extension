9f4434515bfe990ed04c0f97c5c9162cd03b2dca
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2024-05-14 22:03:15 -0300

    doc added to popup.js

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -5,6 +5,10 @@ let content = {
     submit  :document.querySelector("input#submit")
 }
 
+/**
+ * @Called When the submit button is pressed
+ * @Do get the text, send to gemini ai, and activate the alert
+ */
 content.submit.addEventListener("click",sendText)
 function sendText() {
     let prompt = content.textarea.value
@@ -20,12 +24,22 @@ function sendText() {
     });
 }
 
+/**
+ * @Called When a message i sent by the gemini ai
+ * @Do send the message to the content.js to be alerted
+ * @param {String} message 
+ */
 function sendAlert(message) {
     chrome.tabs.query({active:true, currentWindow: true},function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id,message)
     });
 }
 
+/**
+ * @Called When the user change the textarea
+ * @Do Change the height based on its rows and the width based on
+ * the length of the largest string
+ */
 content.textarea.addEventListener("input",checkTextAreaSize)
 function checkTextAreaSize() {
     let text = content.textarea.value
